fe00d3dc72dd76036658333fc4023064c751e4f4
WeiTim_mac <speciallife0318@gmail.com>
2023-01-28 00:54:38 +0800

    new logo

--- src/components/header/HeaderView.vue
+++ src/components/header/HeaderView.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="header-view">
         <div class="logo">
-            <router-link :to="{name: 'HomePage'}"><h3>WeiTim</h3></router-link>
+            <router-link :to="{name: 'HomePage'}">
+                <img src="@/assets/logo.jpeg" alt="logo" id="logo">
+            </router-link>
         </div>
         <div class="icon">
             <img id="link-icon" src="https://img.icons8.com/ios-glyphs/60/null/linkedin-circled--v1.png"/>
@@ -25,7 +27,7 @@ div.header-view {
     display: flex;
     justify-content: space-between;
     background-color:   rgb(255, 255, 255);
-    box-shadow: 0 2px 2px 0 rgba(150, 150, 150, 0.2), 0 4px 4px 0 rgba(150, 150, 150, 0.1);
+    box-shadow: 0 1px 1px 0 rgba(150, 150, 150, 0.2), 0 3px 3px 0 rgba(150, 150, 150, 0.1);
 }
 
 div.icon {
@@ -43,6 +45,18 @@ div.icon {
 a {
     /* padding: 0.5rem; */
     text-decoration: none;
+}
+
+div.logo {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+}
+
+#logo {
+    height: 2rem;
+    padding: 0.6rem;
 }
 
 
